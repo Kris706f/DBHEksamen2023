f4a6e07fdb2059b3d5fed97426fb74d5c5514360
Kris706f <126452879+Kris706f@users.noreply.github.com>
2023-12-13 10:26:02 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ menu_btn.addEventListener('click', function(){
 
 let currentSection = "";
 
+
+
+
+function toggleCards() {
+    var kortElementer = document.querySelectorAll('.proces .cirkel');
+
+    kortElementer.forEach(function (kort) {
+        kort.classList.toggle('synlig');
+    });
+}
